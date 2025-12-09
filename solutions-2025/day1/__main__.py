05ccf41363e5ba44fe08ed71efe568e54a1a5bb7
Will Ockmore <will.ockmore@gmail.com>
2025-12-09 10:18:30 +0800

    wip

--- solutions-2025/day1/__main__.py
+++ solutions-2025/day1/__main__.py
@@ -1,29 +1,47 @@
-from collections import Counter
 from pathlib import Path
 
 with open(Path(__file__).parent / "input.txt") as f:
     lines = f.readlines()
 
-ids = [line.split() for line in lines]
-
-first, second = [list(x) for x in zip(*ids)]
+lines = [line.strip() for line in lines]
+instructions = [(line[0], int(line[1:])) for line in lines if line]
 
 
 def part_1():
-    print(
-        sum(
-            abs(x - y)
-            for x, y in zip(
-                sorted(int(i) for i in first), sorted(int(i) for i in second)
-            )
-        )
-    )
+    # The dial starts on 50.
+    pos = 50
+    times_on_zero = 0
+    for direction, amount in instructions:
+        if direction == "L":
+            pos = ((pos + 10000) - amount) % 100
+        else:
+            pos = (pos + amount) % 100
+
+        if pos == 0:
+            times_on_zero += 1
+    print(times_on_zero)
 
 
 def part_2():
-    count_second = Counter(second)
+    # The dial starts on 50.
+    pos = 50
+    times_on_zero = 0
+    for direction, amount in instructions:
+        if direction == "L":
+            p = pos + 10000
+            d = (p - amount)
 
-    print(sum(int(x) * int(count_second.get(x, 0)) for x in first))
+            if amount >= pos:
+                times_on_zero += (amount-pos) // 100
+
+            pos = d % 100
+        else:
+            d = (pos + amount)
+            times_on_zero += d // 100
+            pos = d % 100
+        print(times_on_zero)
+
+    print(times_on_zero)
 
 
 part_1()
